067df10fb3e0a21e5cc1864ba5dffbe85736043d
peixed351 <peixed351@gmail.com>
2024-01-31 18:48:02 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "OPEDD"
 PROJECT_NAME_FULL: str = "OPEDD: Off-Road Pedestrian Detection Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
